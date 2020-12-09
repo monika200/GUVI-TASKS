40725d431ecb693c731397db98ce8f6b91afe3a7
MONIKA SHARMA <63012770+monika200@users.noreply.github.com>
2020-12-10 04:35:20 +0530

    JS FILE

---
+++ TASK-3/TASK3.5/script.js
@@ -0,0 +1,32 @@
+var request=new XMLHttpRequest();
+
+request.open
+
+('GET','https://restcountries.eu/rest/v2/all', 
+
+true)
+
+request.send();
+
+
+request.onload = function () {
+
+    
+var data=JSON.parse(this.response);
+   
+var usd_currency = data.filter((element) =>{
+ 
+let flag =false 
+element.currencies.forEach((item) =>{
+if (item.code==="USD")
+flag=true;
+})
+
+if(flag==true)
+return true;
+else
+return false;
+
+})
+console.log(usd_currency);
+}
