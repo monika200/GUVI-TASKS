14a6cf94ab9103d7a6ffe1d2d0eb1b7342b2e62e
MONIKA SHARMA <63012770+monika200@users.noreply.github.com>
2020-12-11 12:55:48 +0530

    TASK4.2

--- TASK-4/TASK4.2/script1.js
+++ TASK-4/TASK4.2/script1.js
@@ -8,29 +8,39 @@ request.send();
 request.onload = function() {
 
     var data = JSON.parse(this.response) // object to exract json object from api
+   var lat = data[0].latlng[0];
+   var long = data[0].latlng[1];
+   var nam = data[10].name;
+   getweather(lat ,long);
+   getweathercity(nam);
+}
+function getweathergeo(lat, long){
+var request = new XMLHttpRequest() //object to request Http connection
+//To set api connection true
+var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=f987319a01e8fcb3e169db92403f6e7c';
+request.open('GET', url, true)
+request.send();
+
+// function to load http connection
+
+request.onload = function() {
+
+    var data = JSON.parse(this.response) // object to exract json object from api
     console.log(data);
 }
-var request1 = new XMLHttpRequest() //object to request Http connection
-//To set api connection true
-request1.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Australia&appid=f987319a01e8fcb3e169db92403f6e7c', true)
-request1.send();
-
-// function to load http connection
-
-request1.onload = function() {
-
-    var data1 = JSON.parse(this.response) // object to exract json object from api
-    console.log(data1);
 }
-var request2 = new XMLHttpRequest() //object to request Http connection
+function getweathercityname(nam){
+var request = new XMLHttpRequest() //object to request Http connection
 //To set api connection true
-request2.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=33&lon=65&appid=f987319a01e8fcb3e169db92403f6e7c', true)
-request2.send();
+var url1 = 'http://api.openweathermap.org/data/2.5/weather?q='+nam+'&appid=f987319a01e8fcb3e169db92403f6e7c'
+request.open('GET', url1, true)
+request.send();
 
 // function to load http connection
 
-request2.onload = function() {
+request.onload = function() {
 
-    var data2 = JSON.parse(this.response) // object to exract json object from api
-    console.log(data2);
+    var data = JSON.parse(this.response) // object to exract json object from api
+    console.log(data);
+}
 }
