9b02c39cb9b4acb359c077342d1d8eed341111c6
MONIKA SHARMA <63012770+monika200@users.noreply.github.com>
2020-12-12 15:57:51 +0530

    TASK2.6

--- TASK-2/TASK2.6/TASK2.6.js
+++ TASK-2/TASK2.6/TASK2.6.js
@@ -1,12 +1,12 @@
 //2.6 Return median of two sorted arrays of same size
 
-var array1=[1,2,3,4,5,6,7];
+var array1=[1,2,3,4,5,6];
 var array2=[4,5,7,8,8,9];
 //anonymous function to find median of 2 array
 var medianOfArray=function median(array1,array2){
     var newArray=[...array1,...array2],s,len=newArray.length;
-    newArray.sort(((x,y)=>x-y));
-    s=(newArray[len/2]+newArray[len/2+1])/2
-    return 2;
+    var a = newArray.sort(((x,y)=>x-y));
+    s=(newArray[len/2-1]+newArray[len/2])/2
+    return s;
 }
 console.log(medianOfArray(array1,array2))//2
