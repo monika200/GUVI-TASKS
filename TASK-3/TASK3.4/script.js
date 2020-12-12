e796318373e1d1a3fb3461caa189a6490440d29d
MONIKA SHARMA <63012770+monika200@users.noreply.github.com>
2020-12-12 13:01:48 +0530

    JS FILE

---
+++ TASK-3/TASK3.4/script.js
@@ -0,0 +1,22 @@
+var request=new XMLHttpRequest();
+
+request.open
+
+('GET','https://restcountries.eu/rest/v2/all', 
+
+true)
+
+request.send();
+
+
+request.onload = function () {
+
+    
+var data=JSON.parse(this.response);
+   
+var totalpopulation = data.reduce((a,b) =>{
+  return a+b.population;
+},0)
+
+    console.log(totalpopulation);
+}
